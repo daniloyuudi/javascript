a3cb9f1cec52119114a8c9711d688f872e108fb6
Danilo <dnl496@outlook.com>
2020-11-05 13:50:01 -0300

    Final curso

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,30 @@
+console.log("Buscarei os pacientes");
+
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(event) {
+	console.log("Buscando pacientes");
+
+	var xhr = new XMLHttpRequest();
+
+	xhr.open("GET", "https://api-pacientes.herokuapp.com/pacientesdisadoisadap");
+
+	xhr.addEventListener("load", function() {
+		var erroAjax = document.querySelector("#erro-ajax");
+		if (xhr.status == 200) {
+			erroAjax.classList.add("invisivel");
+			var resposta = xhr.responseText;
+			var pacientes = JSON.parse(resposta);
+
+			pacientes.forEach(function(paciente) {
+				adicionaPacienteNaTabela(paciente);
+			})
+		} else {
+			console.log(xhr.status);
+			console.log(xhr.responseText);
+			erroAjax.classList.remove("invisivel");
+		}
+	})
+
+	xhr.send();
+})
--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -2,6 +2,13 @@ var pacientes = document.querySelectorAll(".paciente");
 
 var tabela = document.querySelector("#tabela-pacientes");
 
-tabela.addEventListener("click", function() {
-	event.target.parentNode.remove();
+tabela.addEventListener("click", function(event) {
+	event.target.parentNode.classList.add("fadeOut");
+
+	setTimeout(function() {
+		if (event.target.tagName == "TD") {
+			event.target.parentNode.remove();
+		}
+	}, 500);
+	//event.target.parentNode.remove();
 })
